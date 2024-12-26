badf2d8ad18ba8fc978e58417811eb4d9ff44cdf
eodudrepublic <eodudrepublic@gmail.com>
2024-12-26 15:54:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# kaist_delivery
+
+카이스트 기숙사에서 배달이 되는 식당을 소개하는 Flutter Application
+
+## 몰입캠프 2024W 4분반
+
+### 김대영 (부산대학교), 최연우 (KAIST) 
---
+++ android/app/src/main/kotlin/immersion_camp/week1/app/kaist_delivery/MainActivity.kt
@@ -0,0 +1,5 @@
+package immersion_camp.week1.app.kaist_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
