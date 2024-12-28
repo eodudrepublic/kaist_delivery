be20b2753c3469ddfecffdbfc9621b3c75e9a9c0
eodudrepublic <eodudrepublic@gmail.com>
2024-12-28 18:35:50 +0900

    feat - 스플래쉬 화면 수정 - flutter_native_splash를 사용하여 스플래쉬 화면 수정 - 스플래쉬 화면 수정을 위해 앱 아이콘 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:kaist_delivery/view/splash_screen.dart';
+import 'package:kaist_delivery/view/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       designSize: const Size(390, 850),
       // TODO : getPages 루트 설정
       child: GetMaterialApp(
-        home: SplashScreen(),
+        home: HomeView(),
       ),
     );
   }
