27f9923de34faba08fba23736ee4a035a45e7bff
yeonwoo <yeonu0681658@gmail.com>
2024-12-28 18:36:03 +0900

    style - 카드 디자인 변경, 바텀 바 디자인 변경

--- lib/view/tab1/widget/restaurant_card.dart
+++ lib/view/tab1/widget/restaurant_card.dart
@@ -13,11 +13,23 @@ class RestaurantCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Container(
       margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+      decoration: BoxDecoration(
+        color: Colors.white, // 배경색
+        borderRadius: BorderRadius.circular(5), // 둥근 모서리
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5), // 그림자 색상
+            blurRadius: 4, // 그림자 흐림 정도
+            offset: Offset(0, 4), // 그림자 위치 (x, y)
+          ),
+        ],
+      ),
       child: ListTile(
         tileColor: Colors.white,
         leading: ClipRRect(
+          borderRadius: BorderRadius.circular(3),
           child: Container(
             width: 0.2.sw, height: 0.2.sw,
             child: Image.asset(
@@ -34,12 +46,11 @@ class RestaurantCard extends StatelessWidget {
           ),
         ),
         subtitle: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 0.01.sh,),
+                SizedBox(height: 0.01.sh),
                 Row(
                   children: [
                     Icon(Icons.phone, size: 16.sp, color: Colors.grey),
@@ -50,7 +61,7 @@ class RestaurantCard extends StatelessWidget {
                     ),
                   ],
                 ),
-                SizedBox(height: 0.01.sh,),
+                SizedBox(height: 0.01.sh),
                 Row(
                   children: [
                     Icon(Icons.access_time, size: 16.sp, color: Colors.grey),
